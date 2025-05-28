b376d8e9181598f96d18d2a144b2f6f86fdd3bfa
Dmytro Ostapovets <dmytro@ostapovets.com>
2025-05-28 11:21:25 +0100

    fix: test

---
+++ utils/docker/publish.js
@@ -0,0 +1,54 @@
+const { execSync } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+
+const projectName = process.argv[2];
+const projectPath = path.join('apps', projectName);
+const dockerfile = path.join(projectPath, "Dockerfile");
+const imageName = `ghcr.io/dostapovets/mono-skaffold-nx-${projectName}`;
+
+if (!projectName) {
+  console.error("Please provide the project name as an argument.");
+  process.exit(1);
+}
+// Read package.json
+const packageJsonPath = path.join(projectPath, "package.json");
+const { version } = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+
+if (!version) {
+  console.error("No version field found in package.json");
+  process.exit(1);
+}
+
+// Split version into major, minor, patch
+const [major, minor] = version.split(".");
+
+// Print as shell export statements
+execSync(
+  `docker build -f ${dockerfile} . --tag ${imageName}:${version} --target production`,
+  { stdio: 'inherit' }
+);
+
+execSync(
+  `docker image tag ${imageName}:${version} ${imageName}:${major}`,
+  { stdio: 'inherit' }
+);
+execSync(
+  `docker image tag ${imageName}:${version} ${imageName}:${major}.${minor}`,
+  { stdio: 'inherit' }
+);
+execSync(
+  `docker push ghcr.io/dostapovets/mono-skaffold-nx-${projectName}:${version}`,
+  { stdio: 'inherit' }
+);
+execSync(
+  `docker push ghcr.io/dostapovets/mono-skaffold-nx-${projectName}:${major}`,
+  { stdio: 'inherit' }
+);
+execSync(
+  `docker push ghcr.io/dostapovets/mono-skaffold-nx-${projectName}:${major}.${minor}`,
+  { stdio: 'inherit' }
+);
+console.log(`Docker image ${imageName}:${version} built and pushed successfully.`);
+console.log(`Docker image ${imageName}/${major} built and pushed successfully.`);
+console.log(`Docker image ${imageName}/${major}:${minor} built and pushed successfully.`);
